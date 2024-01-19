e6b00895a80b24211f933d243f225c4cd0092eb3
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-01-19 17:14:47 -0300

    ;

---
+++ customizing-example/src/components/Intro1.js
@@ -0,0 +1,13 @@
+function Intro1(){
+    return (
+        <div className="blog-post-intro">
+            <h2>I've become a react developer!</h2>
+            <div>
+                <p>I1ve completed React Basics course and I'm happy to announce that I'm now a Junior React Developer!</p>
+                <p className="link">Read more...</p>
+            </div>
+        </div>
+    );
+};
+
+export default Intro1;
