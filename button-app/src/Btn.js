7b689f276c834b0c84fffbe484d319ee0d108bc8
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-01-22 16:44:00 -0300

    Update Btn.js

--- button-app/src/Btn.js
+++ button-app/src/Btn.js
@@ -1,7 +1,18 @@
-function Btn(){
-    const clickHandler = () => console.log('clicked')
+/*function Btn(){
+    const clickHandler = () => console.log('c')
     return(
         <button onClick={clickHandler}>
+        Click Me
+        </button>
+    );
+}
+
+export default Btn;*/ 
+
+function Btn(){
+    const clickHandler = () => console.log('mouse over')
+    return(
+        <button onMouseOver={clickHandler}>
         Click Me
         </button>
     );
