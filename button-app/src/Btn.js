d1ff1d5dc2f665e455c65e31c279696f1d976122
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-01-22 18:21:07 -0300

    Update Btn.js

--- button-app/src/Btn.js
+++ button-app/src/Btn.js
@@ -1,5 +1,5 @@
 /*function Btn(){
-    const clickHandler = () => console.log('c')
+    const clickHandler = () => console.log('CLICKED')
     return(
         <button onClick={clickHandler}>
         Click Me
@@ -10,11 +10,34 @@
 export default Btn;*/ 
 
 function Btn(){
-    const clickHandler = () => console.log('mouse over')
+    function thirdExample() {
+        console.log('third example'); };
+    const clickHandler = () => console.log('mouse over');
+    const clickHandler2 = () => console.log('CLICKED');
     return(
-        <button onMouseOver={clickHandler}>
-        Click Me
-        </button>
+        <div>
+            <button onMouseOver={clickHandler}>
+            Click Me
+            </button>
+            <br/>
+            <br/>
+            <button onClick={() => console.log('second example')}>
+                An inline anonymous ES6 function event handler
+            </button>
+            <br/>
+            <br/>
+            <button onClick={thirdExample}>
+                 using a separate function declaration
+            </button>
+            <br/>
+            <br/>
+
+            <button onClick={clickHandler2}>
+               Click Me
+            </button>
+        </div>
+
+
     );
 }
 
